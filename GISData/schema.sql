4eb3959b4129175bccb933b208b44b5a3d5f8eb1
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 03:11:34 +0100

    test of SQL export

---
+++ GISData/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model GISData of the subject dataModel.RiskManagement for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE GISData_type AS ENUM ('GISData');
+CREATE TABLE GISData (address json, alternateName text, analyzedAt timestamp, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, identifiesVulnerability text, isMappedBy json, location json, name text, owner json, seeAlso json, source text, type GISData_type);
