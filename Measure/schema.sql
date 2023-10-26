1c06d40a6c8f4f6c285af5833d80996f2e9bf1bd
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:05:26 +0200

    test of SQL export

--- Measure/schema.sql
+++ Measure/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Measure of the subject dataModel.RiskManagement for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Measure_type AS ENUM ('Measure');
-CREATE TABLE Measure (action JSON, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type Measure_type);
+CREATE TABLE Measure (action JSON, address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type Measure_type);
