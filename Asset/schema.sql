2e2f1618cdbc1906761d725950269fbe14d7adf8
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:05:14 +0200

    test of SQL export

--- Asset/schema.sql
+++ Asset/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Asset of the subject dataModel.RiskManagement for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Asset_type AS ENUM ('Asset');
-CREATE TABLE Asset (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type Asset_type);
+CREATE TABLE Asset (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, seeAlso JSON, source TEXT, type Asset_type);
